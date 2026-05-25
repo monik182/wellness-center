9ec86e254fc9c037c3d2bb04dd28ef9774874b2d
Monica Carvajal <monik.182@gmail.com>
2026-05-25 23:03:10 +0200

    create gh action

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? "/wellness-center/" : "/",
   build: { outDir: "dist" },
 });
