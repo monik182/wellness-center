20d0694af49be8ebd91fa4a4ef7f9b8744bc622a
Monica Carvajal <monik.182@gmail.com>
2026-06-22 21:19:11 +0200

    clean up tabs

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import "./styles/global.css";
 
-const MealsPage = lazy(() => import("./pages/MealsPage"));
-const SchedulePage = lazy(() => import("./pages/SchedulePage"));
-const WheelPage = lazy(() => import("./pages/WheelPage"));
 const FoodsPage = lazy(() => import("./pages/FoodsPage"));
-const PrepPage = lazy(() => import("./pages/PrepPage"));
-const ShoppingPage = lazy(() => import("./pages/ShoppingPage"));
-const RulesPage = lazy(() => import("./pages/RulesPage"));
-const MacroTargetsPage = lazy(() => import("./pages/MacroTargetsPage"));
+const SchedulePage = lazy(() => import("./pages/SchedulePage"));
 const CalorieTrackerPage = lazy(() => import("./pages/CalorieTrackerPage"));
 const NutritionLabelPage = lazy(() => import("./pages/NutritionLabelPage"));
 const SaludMetabolicaPage = lazy(() => import("./pages/SaludMetabolicaPage"));
+// const MealsPage = lazy(() => import("./pages/MealsPage"));
+// const WheelPage = lazy(() => import("./pages/WheelPage"));
+// const PrepPage = lazy(() => import("./pages/PrepPage"));
+// const ShoppingPage = lazy(() => import("./pages/ShoppingPage"));
+// const RulesPage = lazy(() => import("./pages/RulesPage"));
+// const MacroTargetsPage = lazy(() => import("./pages/MacroTargetsPage"));
 
 const LoadingFallback = () => (
   <div className="flex items-center justify-center h-32">
@@ -27,16 +27,16 @@ export default function App() {
       <Layout>
         <Suspense fallback={<LoadingFallback />}>
           <Routes>
-            <Route path="/" element={<MealsPage />} />
+            {/* <Route path="/" element={<MealsPage />} /> */}
+            <Route path="/" element={<CalorieTrackerPage />} />
             <Route path="/schedule" element={<SchedulePage />} />
-            <Route path="/wheel" element={<WheelPage />} />
+            {/* <Route path="/wheel" element={<WheelPage />} /> */}
             <Route path="/foods" element={<FoodsPage />} />
-            <Route path="/prep" element={<PrepPage />} />
-            <Route path="/shopping" element={<ShoppingPage />} />
+            {/* <Route path="/prep" element={<PrepPage />} /> */}
+            {/* <Route path="/shopping" element={<ShoppingPage />} /> */}
             <Route path="/nutrition-label" element={<NutritionLabelPage />} />
-            <Route path="/rules" element={<RulesPage />} />
-            <Route path="/macro-targets" element={<MacroTargetsPage />} />
-            <Route path="/calorie-tracker" element={<CalorieTrackerPage />} />
+            {/* <Route path="/rules" element={<RulesPage />} /> */}
+            {/* <Route path="/macro-targets" element={<MacroTargetsPage />} /> */}
             <Route path="/salud-metabolica" element={<SaludMetabolicaPage />} />
           </Routes>
         </Suspense>
